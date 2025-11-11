7c1bd5da41705a44b7cfa4b0fb69a79c09beb183
Dilton Thales <diltonthales.dt@gmail.com>
2025-11-10 22:36:59 -0300

    fat: adicao home page

---
+++ cypress/e2e/home.cy.js
@@ -0,0 +1,3 @@
+//fazer testes da página home, como login, home, ações como consultar produto e ver produtos
+//Padrão page ougts, cada página vira um objeto (atributos e comportamentos)
+// Mapeia e reutiliza isso nos testes
