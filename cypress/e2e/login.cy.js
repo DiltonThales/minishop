8ae5ed7c237ae22de91307f43a99859860eb078c
Dilton Thales <diltonthales.dt@gmail.com>
2025-11-10 22:11:17 -0300

    Assercao

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -9,10 +9,13 @@ describe('Testes de login do MiniShop', () => {
     cy.title().should('be.eq', 'MiniShop - Login')
   })
 
-  it('Login com campos em branco', () => {
+  it('Login com campos em branco', () => { // até aqui não temos um teste, verifica o resultado que tive com o resultado que estava esperando
     cy.get('#username').clear()
     cy.get('#password').clear()  
-    cy.get('#button[type=submit]').click
+    cy.get('button[type=submit]').click()
+
+    // Asserção
+    cy.get('div[role=alert]').should('be.visible')
   })
 
 })
