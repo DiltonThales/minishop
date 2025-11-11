e131f5682f18ba8d17f5c3f62d7e8fc3b80d2e8e
Dilton Thales <diltonthales.dt@gmail.com>
2025-11-10 21:58:30 -0300

    fat: adicao dos it

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,6 +1,16 @@
 describe('Testes de login do MiniShop', () => {
 
+  beforeEach(() => {
+    cy.visit('./html/index.html')
+  })
+
   it('Verifica o titulo da aba da página', () => {
+    cy.visit('./html/index.html')
+    cy.title().should('be.eq', 'MiniShop - Login')
+  })
+
+  it('Login com campos em branco', () => {
+    
 
   })
 
