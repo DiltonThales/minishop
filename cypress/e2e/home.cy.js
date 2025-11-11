5467c26995bbcb0a350d306d11d9ad5e06287d8c
Dilton Thales <diltonthales.dt@gmail.com>
2025-11-10 22:44:46 -0300

    fat: adicao da home describe

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,3 +1,10 @@
 //fazer testes da página home, como login, home, ações como consultar produto e ver produtos
 //Padrão page ougts, cada página vira um objeto (atributos e comportamentos)
 // Mapeia e reutiliza isso nos testes
+
+import { HomePage } from "../support/pages/HomePage"
+
+describe('Página Home da Aplicação MiniShop', () => {
+    
+    const home = new HomePage
+})
--- cypress/support/pages/HomePage.js
+++ cypress/support/pages/HomePage.js
@@ -1,3 +1,15 @@
 export class HomePage{
-    
+    // Separa Seletores (atributos)
+    botaoSair = () => cy.contains('button', 'Sair').should("exist")
+    listaProdutos = () => cy.get('#product-list')
+    title = 'MiniShop - Home'
+
+    //Ações (metodos)
+    visitar(){
+        cy.visit('./html/home.html')
+    }
+
+    verificarTitulo(){
+        cy.title().should('be.aq', this.title)
+    }
 }
