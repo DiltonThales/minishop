fbc4d5b52bbe6178b77a9f4e2df71e871d68ee7a
Dilton Thales <diltonthales.dt@gmail.com>
2025-11-11 19:34:25 -0300

    refactor: ajustes

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -10,17 +10,12 @@ describe('Página Home da Aplicação MiniShop', () => {
     const home = new HomePage()
     const loginPage = new LoginPage()
     
-
     beforeEach(() => {
         home.visitar()
         loginPage.login('admin', '12345')
-        
-
     })
 
     it('Deve exibir o titulo correto', () =>{
         home.verificarTitulo
-        
-
     })
 })
